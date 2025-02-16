3d3b82d9f4a0320f20af25eb70a163becf4847e9
Daniel Mabadeje <mabadejedaniel1@gmail.com>
2025-02-16 13:36:26 +0100

    added ethereum.php in config

---
+++ config/ethereum.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'rpc_url' => env('ETH_RPC_URL', 'https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID'),
+    'escrow_contract' => env('ETH_ESCROW_CONTRACT'),
+    'private_key' => env('ETH_PRIVATE_KEY'),
+    'owner_address' => env('ETH_OWNER_ADDRESS'),
+    'abi' => file_get_contents(base_path('storage/app/public/ethereum_abi.json')),
+    // 'abi' => '',
+];
